7c9007ef4bea78b1234ff01a823351c88ce210ed
Zeyad Mohammed <zeyadmohammed200493@gmail.com>
2025-12-27 01:01:22 +0200

    feat(brand): add update brand endpoint

--- src/modules/brand/brand.controller.ts
+++ src/modules/brand/brand.controller.ts
@@ -1,11 +1,21 @@
 import { Auth } from "src/common/decorators/auth.decorator";
 import { BrandService } from "./brand.service";
-import { Body, Controller, Post, Req } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from "@nestjs/common";
 import { Image, RoleEnum, UploadImage } from "src/common";
 import { User } from "src/common/decorators/credential.decorator";
 import type { UserDocument } from "src/DB";
 import { CreateBrandDto } from "./dto/create-brand.dto";
 import type { Request } from "express";
+import { UpdateBrandDto } from "./dto/update-brand.dto";
+import { Types } from "mongoose";
 
 @Controller("brand")
 export class BrandController {
@@ -18,8 +28,26 @@ export class BrandController {
     @Body() body: CreateBrandDto,
     @User() user: UserDocument,
     @Image() image?: Express.Multer.File,
-  ): Promise<string> {
-    await this.brandService.createBrand(body, image || undefined, user);
-    return "Done";
+  ): Promise<{ message: string }> {
+    await this.brandService.createBrand({
+      body,
+      image,
+      user,
+    });
+    return { message: "Done" };
+  }
+
+  @Auth([RoleEnum.admin])
+  @Patch("/:id")
+  @UploadImage()
+  async updateBrand(
+    @Param('id') id: Types.ObjectId,
+    @Body() body: UpdateBrandDto,
+    @User() user: UserDocument,
+    @Image() image?: Express.Multer.File,
+  ): Promise<{ message: string }> {
+    console.log({ id, body, image, user });
+    await this.brandService.updateBrand({ id, body, image, user });
+    return { message: "Done" };
   }
 }
--- src/modules/brand/brand.service.ts
+++ src/modules/brand/brand.service.ts
@@ -1,17 +1,28 @@
 import { BrandRepository } from "./../../DB/repository/brand.repository";
-import { BadRequestException, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { CreateBrandDto } from "./dto/create-brand.dto";
 import { Brand, UserDocument } from "src/DB";
+import { Types } from "mongoose";
+import { UpdateBrandDto } from "./dto/update-brand.dto";
+import fs from "fs/promises";
 
 @Injectable()
 export class BrandService {
   constructor(private readonly brandRepository: BrandRepository) {}
 
-  async createBrand(
-    body: CreateBrandDto,
-    image: Express.Multer.File | undefined,
-    user: UserDocument,
-  ) {
+  async createBrand({
+    body,
+    image,
+    user,
+  }: {
+    body: CreateBrandDto;
+    image?: Express.Multer.File;
+    user: UserDocument;
+  }): Promise<string> {
     const brandData: Partial<Brand> = {
       name: body.name,
       image: image?.path,
@@ -27,5 +38,52 @@ export class BrandService {
     }
 
     const brand = await this.brandRepository.create({ data: [brandData] });
+
+    if (!brand) {
+      throw new BadRequestException("Failed to create brand");
+    }
+
+    return "Done";
+  }
+  async updateBrand({
+    id,
+    body,
+    image,
+    user,
+  }: {
+    id: Types.ObjectId;
+    body: UpdateBrandDto;
+    image?: Express.Multer.File;
+    user: UserDocument;
+  }): Promise<string> {
+    const brand = await this.brandRepository.findOne({
+      filter: { _id: id, createdBy: user._id },
+    });
+    if (!brand) {
+      throw new NotFoundException(
+        "Brand not found or unAuthorized to update it",
+      );
+    }
+
+    if (body.name && body.name !== brand.name) {
+      const isNameExist = await this.brandRepository.findOne({
+        filter: { name: body.name },
+      });
+
+      if (isNameExist) {
+        throw new BadRequestException("Brand name already used");
+      }
+      brand.name = body.name;
+    }
+    if (image?.path) {
+      if (brand.image) {
+        await fs.unlink(brand.image);
+      }
+      brand.image = image.path;
+    }
+
+    await brand.save();
+
+    return "Done";
   }
 }
