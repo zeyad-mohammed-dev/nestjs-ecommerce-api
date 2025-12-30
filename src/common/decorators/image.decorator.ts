ca53910fe9158d1c46c0b6cabd5e3a4a4fd305a5
Zeyad Mohammed <zeyadmohammed200493@gmail.com>
2025-12-30 14:22:31 +0200

    refactor(common): support single and multiple file uploads

--- src/common/decorators/image.decorator.ts
+++ src/common/decorators/image.decorator.ts
@@ -6,3 +6,14 @@ export const Image = createParamDecorator(
     return request.file;
   },
 );
+
+export const Images = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    if (request?.files) {
+      const imagesPaths = request.files.map((file) => file.path);
+      return imagesPaths;
+    }
+    return [];
+  },
+);
