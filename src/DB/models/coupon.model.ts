3c0fef2e7d7ac3912aea6533c9eb3721159787f6
Zeyad Mohammed <zeyadmohammed200493@gmail.com>
2026-01-02 23:24:59 +0200

    feat(coupon): add coupon model

---
+++ src/DB/models/coupon.model.ts
@@ -0,0 +1,30 @@
+import { HydratedDocument } from "mongoose";
+import { MongooseModule, Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({ timestamps: true })
+export class Coupon {
+  @Prop({ type: String, required: true })
+  code: string;
+
+  @Prop({ type: Number, required: true })
+  discount: number;
+
+  @Prop({ type: Date, required: true })
+  expiredIn: Date;
+
+  @Prop({ type: Number, default: 0 })
+  totalUsage: number;
+
+  @Prop({ type: Number, default: 10 })
+  maxUsage: number;
+}
+export type CouponDocument = HydratedDocument<Coupon>;
+
+const couponSchema = SchemaFactory.createForClass(Coupon);
+
+export const CouponModel = MongooseModule.forFeature([
+  {
+    name: Coupon.name,
+    schema: couponSchema,
+  },
+]);
