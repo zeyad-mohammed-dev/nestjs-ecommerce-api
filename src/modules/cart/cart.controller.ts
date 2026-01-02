e79dbc562f96de4fb912e3862422769ab8812cd5
Zeyad Mohammed <zeyadmohammed200493@gmail.com>
2026-01-02 22:50:49 +0200

    feat(cart): add add-to-cart endpoint

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -1,7 +1,21 @@
-import { Controller, Post } from "@nestjs/common";
+import type { UserDocument } from "./../../DB/models/user.model";
+import { Body, Controller, Post } from "@nestjs/common";
 import { CartService } from "./cart.service";
+import { Auth } from "src/common/decorators/auth.decorator";
+import { RoleEnum } from "src/common";
+import { User } from "src/common/decorators/credential.decorator";
+import { AddToCartDto } from "./dto/add-to-cart.dto";
 
 @Controller("cart")
 export class CartController {
   constructor(private readonly cartService: CartService) {}
+
+  @Auth([RoleEnum.user, RoleEnum.admin])
+  @Post("add-to-cart")
+  async addToCart(
+    @User() user: UserDocument,
+    @Body() productData: AddToCartDto,
+  ) {
+    return this.cartService.addToCart({ user, productData });
+  }
 }
--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -1,4 +1,96 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import {
+  CartDocument,
+  CartRepository,
+  ProductRepository,
+  UserDocument,
+} from "src/DB";
+import { AddToCartDto } from "./dto/add-to-cart.dto";
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(
+    private readonly productRepository: ProductRepository,
+    private readonly cartRepository: CartRepository,
+  ) {}
+
+  async addToCart({
+    user,
+    productData,
+  }: {
+    user: UserDocument;
+    productData: AddToCartDto;
+  }) {
+    //check if product exists and in stock
+    const product = await this.productRepository.findOne({
+      filter: {
+        _id: productData.productId,
+        stock: {
+          $gte: productData.quantity,
+        },
+      },
+    });
+    if (!product) {
+      throw new BadRequestException("Product not found or out of stock");
+    }
+
+    //check if user has a cart
+    const userCart = await this.cartRepository.findOne({
+      filter: {
+        userId: user._id,
+      },
+    });
+
+    if (!userCart) {
+      //create new cart with item data
+      const cart = await this.cartRepository.create({
+        data: [
+          {
+            userId: user._id,
+            items: [
+              {
+                productId: productData.productId,
+                quantity: productData.quantity,
+              },
+            ],
+          },
+        ],
+      });
+      //check if cart created successfully
+      if (cart) {
+        return "added to cart";
+      } else {
+        throw new BadRequestException("Failed to add to cart");
+      }
+    } else {
+      //if user has a cart find the product index
+      const productIndex = userCart.items.findIndex(
+        (item) =>
+          item.productId.toString() === productData.productId.toString(),
+      );
+      //if product not found in cart push it to items and save and return added
+      if (productIndex === -1) {
+        userCart.items.push({
+          productId: productData.productId,
+          quantity: productData.quantity,
+        });
+        await userCart.save();
+        return "added to cart";
+      } else {
+        //if product found in cart
+        const foundItem = userCart.items[productIndex];
+        //if quantity + productData.quantity > product.stock throw error and update quantity with available stock
+        if (foundItem.quantity + productData.quantity > product.stock) {
+          userCart.items[productIndex].quantity = product.stock;
+          await userCart.save();
+          throw new BadRequestException(`Available stock is ${product.stock}`);
+        } else {
+          //if quantity + productData.quantity <= product.stock update quantity and save and return added
+          userCart.items[productIndex].quantity += productData.quantity;
+          await userCart.save();
+          return "added to cart";
+        }
+      }
+    }
+  }
+}
