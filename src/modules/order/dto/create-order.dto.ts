6dc14c62946b02d8033cc89402fb3f62975480b0
Zeyad Mohammed <zeyadmohammed200493@gmail.com>
2026-01-03 21:07:36 +0200

    feat(order): implement create order (checkout) flow

--- src/modules/order/dto/create-order.dto.ts
+++ src/modules/order/dto/create-order.dto.ts
@@ -1,25 +1,11 @@
-import { Type } from "class-transformer";
 import {
   IsArray,
   IsEnum,
   IsNotEmpty,
-  IsNumber,
   IsOptional,
   IsString,
-  Min,
-  ValidateNested,
 } from "class-validator";
 import { PaymentMethodEnum } from "./../../../DB/models/order.model";
-
-export class CreateOrderItemDto {
-  @IsString()
-  @IsNotEmpty()
-  productId: string;
-
-  @IsNumber()
-  @Min(1)
-  quantity: number;
-}
 
 export class CreateOrderDto {
   @IsString()
@@ -38,11 +24,6 @@ export class CreateOrderDto {
   @IsOptional()
   @IsEnum(PaymentMethodEnum)
   paymentMethod?: PaymentMethodEnum;
-
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => CreateOrderItemDto)
-  items: CreateOrderItemDto[];
 
   @IsOptional()
   @IsString()
--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,14 +1,24 @@
+import { Product, ProductDocument } from "./../../DB/models/product.model";
 import { CouponRepository } from "src/DB/repository/coupon.repository";
 import type { UserDocument } from "./../../DB/models/user.model";
-import { Injectable, NotFoundException } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { OrderRepository } from "src/DB/repository/order.repository";
 import { CreateOrderDto } from "./dto/create-order.dto";
+import { CouponDocument } from "src/DB/models/coupon.model";
+import { CartDocument, CartRepository, ProductRepository } from "src/DB";
+import { OrderStatusEnum } from "src/DB/models/order.model";
 
 @Injectable()
 export class OrderService {
   constructor(
     private readonly orderRepository: OrderRepository,
     private readonly couponRepository: CouponRepository,
+    private readonly cartRepository: CartRepository,
+    private readonly productRepository: ProductRepository,
   ) {}
 
   async createOrder({
@@ -19,14 +29,81 @@ export class OrderService {
     orderData: CreateOrderDto;
   }) {
     const code = orderData.coupon;
+    let discountPercent = 0;
     if (code) {
-      const isCouponExist = this.couponRepository.findOne({
+      const coupon = (await this.couponRepository.findOne({
         filter: { code },
-      });
-      if (!isCouponExist) {
+      })) as CouponDocument;
+      if (!coupon) {
         throw new NotFoundException("Coupon not found");
+      } else if (
+        coupon.expiredIn < new Date(Date.now()) ||
+        coupon.maxUsage == coupon.totalUsage
+      ) {
+        throw new BadRequestException("Coupon is expired");
       }
-      else if(isCouponExist.expired)
+      discountPercent = coupon?.discount == 0 ? 0 : coupon?.discount / 100;
+      coupon.totalUsage += 1;
+      await coupon.save();
     }
+
+    const userCart = (await this.cartRepository.findOne({
+      filter: { userId: user._id },
+      options: {
+        populate: [
+          {
+            path: "items.productId",
+            model: Product.name,
+          },
+        ],
+      },
+    })) as CartDocument;
+
+    if (!userCart || userCart.items.length == 0) {
+      throw new BadRequestException("Cart is empty");
+    }
+    const subTotal = userCart.items.reduce((acc, item) => {
+      return (
+        acc +
+        (item.productId as unknown as ProductDocument).salePrice * item.quantity
+      );
+    }, 0);
+    const discount = subTotal * discountPercent;
+    const total = subTotal - discount;
+
+    for (const item of userCart.items) {
+      const product = (await this.productRepository.findById({
+        id: item.productId,
+      })) as ProductDocument;
+      if (!product) {
+        throw new NotFoundException("Product not found");
+      }
+      if (product.stock < item.quantity) {
+        throw new BadRequestException("Not enough stock");
+      }
+      product.stock -= item.quantity;
+      await product.save();
+    }
+
+    const order = await this.orderRepository.create({
+      data: [
+        {
+          userId: user._id,
+          items: userCart.items,
+          subTotal,
+          discount,
+          total,
+          address: orderData.address,
+          instructions: orderData.instructions,
+          phone: orderData.phone,
+          paymentMethod: orderData.paymentMethod,
+          orderStatus: OrderStatusEnum.PENDING,
+        },
+      ],
+    });
+
+    await this.cartRepository.deleteOne({ filter: { userId: user._id } });
+
+    return order;
   }
 }
